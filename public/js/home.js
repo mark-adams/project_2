278fae4da66987b98550ded5f3910af90f8af491
Ted <ted_hodges@ymail.com>
2018-05-10 19:36:03 -0500

    minor tweaks

--- public/js/home.js
+++ public/js/home.js
@@ -17,15 +17,4 @@ $(document)
     $('.ui.sidebar')
       .sidebar('attach events', '.toc.item');
 
-    // lazy load images
-    $('.image').visibility({
-      type: 'image',
-      transition: 'vertical flip in',
-      duration: 500
-    });
-
-    // show dropdown on hover
-    $('.main.menu  .ui.dropdown').dropdown({
-      on: 'hover'
-    });
   });
