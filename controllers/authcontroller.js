eb2886e1573c427545bb6325857164f004e02103
Kiley Adams <Kileyadams92@gmail.com>
2018-05-15 18:17:27 -0500

    continued development of passportjs

---
+++ controllers/authcontroller.js
@@ -0,0 +1,9 @@
+var exports = (module.exports = {});
+
+exports.signup = function(req, res) {
+  res.render("../public/views/signup.html");
+};
+
+exports.signin = function(req, res) {
+  res.render("../public/views/signup.html");
+};
